66aa4c2210a625d954f655b626e2e6807e51c583
sergej jurecko <sergej.jurecko@wpn.tv>
2018-05-15 13:12:55 +0200

    fix example

--- examples/ifconfig-simple.rs
+++ examples/ifconfig-simple.rs
@@ -3,34 +3,27 @@ extern crate interfaces;
 use std::iter;
 use std::net;
 
-use interfaces::{Interface, Kind, flags};
-
+use interfaces::{Interface, Kind, flags::{self, InterfaceFlags}};
 
 // Flag mappings that ifconfig uses, in order.
 const NAME_MAPPINGS: &'static [(flags::InterfaceFlags, &'static str)] = &[
-    (flags::IFF_UP, "UP"),
-    (flags::IFF_LOOPBACK, "LOOPBACK"),
-    (flags::IFF_BROADCAST, "BROADCAST"),
+    (InterfaceFlags::IFF_UP, "UP"),
+    (InterfaceFlags::IFF_LOOPBACK, "LOOPBACK"),
+    (InterfaceFlags::IFF_BROADCAST, "BROADCAST"),
     // SMART?
-    (flags::IFF_RUNNING, "RUNNING"),
+    (InterfaceFlags::IFF_RUNNING, "RUNNING"),
     // SIMPLEX?
-    (flags::IFF_MULTICAST, "MULTICAST"),
+    (InterfaceFlags::IFF_MULTICAST, "MULTICAST"),
 ];
-
 
 fn main() {
     let mut ifs = Interface::get_all().expect("could not get interfaces");
     ifs.sort_by(|a, b| a.name.cmp(&b.name));
 
     // Find the maximum alignment for our interface names.
-    let max_align = ifs.iter()
-        .map(|i| i.name.len() + 2)
-        .max()
-        .unwrap();
+    let max_align = ifs.iter().map(|i| i.name.len() + 2).max().unwrap();
 
-    let full_align = iter::repeat(' ')
-        .take(max_align)
-        .collect::<String>();
+    let full_align = iter::repeat(' ').take(max_align).collect::<String>();
 
     for i in ifs.iter() {
         let name_align = iter::repeat(' ')
@@ -40,10 +33,7 @@ fn main() {
         // Build the first line by printing the interface flags.
         let first_line = {
             let mut buf = String::new();
-            buf.push_str(&*format!(
-                "flags={} <",
-                i.flags.bits()
-            ));
+            buf.push_str(&*format!("flags={} <", i.flags.bits()));
 
             let mut flag_strs = vec![];
             for &(f, s) in NAME_MAPPINGS.iter() {
@@ -53,17 +43,14 @@ fn main() {
             }
 
             buf.push_str(&*flag_strs.join(","));
-            buf.push_str(&*format!(
-                "> mtu {}",
-                i.get_mtu().unwrap_or(0)
-            ));
+            buf.push_str(&*format!("> mtu {}", i.get_mtu().unwrap_or(0)));
 
             buf
         };
 
         println!("{}: {}{}", i.name, name_align, first_line);
 
-        if i.flags.contains(flags::IFF_LOOPBACK) {
+        if i.flags.contains(InterfaceFlags::IFF_LOOPBACK) {
             println!("{}loop (Local Loopback)", full_align);
         } else {
             if let Ok(addr) = i.hardware_addr() {
@@ -90,11 +77,7 @@ fn main() {
 
 fn format_addr(addr: &net::SocketAddr) -> String {
     match addr {
-        &net::SocketAddr::V4(ref a) => {
-            format!("{}", a.ip())
-        },
-        &net::SocketAddr::V6(ref a) => {
-            format!("{}", a.ip())
-        },
+        &net::SocketAddr::V4(ref a) => format!("{}", a.ip()),
+        &net::SocketAddr::V6(ref a) => format!("{}", a.ip()),
     }
 }
